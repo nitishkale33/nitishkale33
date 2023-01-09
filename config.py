9d5062688d83616f1d696b7e3c4390d12b7a74fd
nitishkale33 <86880500+nitishkale33@users.noreply.github.com>
2023-01-09 14:54:58 +0530

    new config

---
+++ config.py
@@ -0,0 +1,11 @@
+import psycopg2
+
+
+def connection():
+    connection = psycopg2.connect(
+        user="postgres",
+        password="Nitish",
+        host="localhost",
+        port="5432",
+        database="postgres")
+    return connection
